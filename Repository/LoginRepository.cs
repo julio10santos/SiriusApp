84228c0a4a95b08c6c82aafe38308efa1232d13c
Julio Santos <juliomjk@hotmail.com.br>
2018-11-21 07:25:23 -0200

    02-Inicio

--- Repository/LoginRepository.cs
+++ Repository/LoginRepository.cs
@@ -72,5 +72,93 @@ namespace Repository
             }
             return usuarioLogado;
         }
+
+        public bool AddUsuario(UsuarioDomain usuario)
+        {
+            var status = false;
+            try
+            {
+                cn = new NpgsqlConnection(StringConnection.ConnectionApp);
+                var query = new StringBuilder();
+                query.Append("UPDATE users SET name=@name, username=@username, email=@email, password_digest=@password, photo=@photo, description=@description, client_id=@client_id, supervisor_id=@supervisor_id, phone_number=@phone_number, created_at=@created_at, updated_at=@updated_at, reset_token=@reset_token, active=@active, confirm_token=@confirm_token, plan=@plan ");
+                query.Append("WHERE  id=@id;");
+                var cmd = new NpgsqlCommand() { CommandType = System.Data.CommandType.Text };
+                cmd.Parameters.AddWithValue("@id", usuario.Id);
+                cmd.Parameters.AddWithValue("@name", usuario.Name);
+                cmd.Parameters.AddWithValue("@username", usuario.Username);
+                cmd.Parameters.AddWithValue("@email", usuario.Email);
+                cmd.Parameters.AddWithValue("@password", usuario.Password_digest);
+                cmd.Parameters.AddWithValue("@photo", usuario.Photo);
+                cmd.Parameters.AddWithValue("@description", usuario.Description);
+                cmd.Parameters.AddWithValue("@client_id", usuario.Client_id);
+                cmd.Parameters.AddWithValue("@supervisor_id", usuario.Supervisor_id);
+                cmd.Parameters.AddWithValue("@phone_number", usuario.Phone_number);
+                cmd.Parameters.AddWithValue("@created_at", usuario.Created_at);
+                cmd.Parameters.AddWithValue("@updated_at", usuario.Updated_at);
+                cmd.Parameters.AddWithValue("@reset_token", usuario.Reset_token);
+                cmd.Parameters.AddWithValue("@active", usuario.Active);
+                cmd.Parameters.AddWithValue("@confirm_token", usuario.Confirm_token);
+                cmd.Parameters.AddWithValue("@plan", usuario.Plan);
+                cn.Open();
+                int codigo = cmd.ExecuteNonQuery();
+                if(codigo == -1){
+                    status = false;
+                }
+                else 
+                {
+                    status = true;
+                }
+            }
+            catch(NpgsqlException ex)
+            {
+                throw ex;
+            }
+            return status;
+        }
+
+        public bool UpdUsuario(UsuarioDomain usuario)
+        {
+            var status = false;
+            try
+            {
+                cn = new NpgsqlConnection(StringConnection.ConnectionApp);
+                var query = new StringBuilder();
+                query.Append("UPDATE users set ( id, name, username, email, password_digest, photo, description, client_id, supervisor_id, phone_number, created_at, updated_at, reset_token, active, confirm_token, plan) ");
+                query.Append("values (@id, @name, @username, @email, @password, @photo, @description, @client_id, @supervisor_id, @phone_number, @created_at, @updated_at, @reset_token, @active, @confirm_token, @plan);");
+                var cmd = new NpgsqlCommand() { CommandType = System.Data.CommandType.Text };
+                cmd.Parameters.AddWithValue("@id", usuario.Id);
+                cmd.Parameters.AddWithValue("@name", usuario.Name);
+                cmd.Parameters.AddWithValue("@username", usuario.Username);
+                cmd.Parameters.AddWithValue("@email", usuario.Email);
+                cmd.Parameters.AddWithValue("@password", usuario.Password_digest);
+                cmd.Parameters.AddWithValue("@photo", usuario.Photo);
+                cmd.Parameters.AddWithValue("@description", usuario.Description);
+                cmd.Parameters.AddWithValue("@client_id", usuario.Client_id);
+                cmd.Parameters.AddWithValue("@supervisor_id", usuario.Supervisor_id);
+                cmd.Parameters.AddWithValue("@phone_number", usuario.Phone_number);
+                cmd.Parameters.AddWithValue("@created_at", usuario.Created_at);
+                cmd.Parameters.AddWithValue("@updated_at", usuario.Updated_at);
+                cmd.Parameters.AddWithValue("@reset_token", usuario.Reset_token);
+                cmd.Parameters.AddWithValue("@active", usuario.Active);
+                cmd.Parameters.AddWithValue("@confirm_token", usuario.Confirm_token);
+                cmd.Parameters.AddWithValue("@plan", usuario.Plan);
+                cn.Open();
+                int codigo = cmd.ExecuteNonQuery();
+                if (codigo == -1)
+                {
+                    status = false;
+                }
+                else
+                {
+                    status = true;
+                }
+            }
+            catch (NpgsqlException ex)
+            {
+                throw ex;
+            }
+            return status;
+        }
+
     }
 }
